8bcb4bc5314ae543010b06cb193a2899275fbcb7
vasyaivanov <mac@Macs-MacBook-Air.local>
2015-12-20 15:37:03 -0800

    App displays list and filters

---
+++ APP/gstlst/platforms/ios/www/js/index.js
@@ -0,0 +1,12 @@
+var guestlistMetadata = {
+    eventName: "Ruby Skye New Years party",
+    guests: [
+        {"guestName":"John"}, 
+        {"guestName":"Anna",}, 
+        {"guestName":"Peter"}
+    ]
+}
+
+var guest= $( "<div id='object1'>asdfasdfasdf</div>" );
+
+$("#guesttable").append(guest);
