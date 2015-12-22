7c6478c70a88e65b22d8fd16b232ffb7b19f99c2
Konstantin <kraskoshnyy@gmail.com>
2015-12-22 13:00:36 -0800

    Added loading screen

--- APP/gstlst/platforms/ios/www/js/index.js
+++ APP/gstlst/platforms/ios/www/js/index.js
@@ -1,88 +1,98 @@
-var socket = io.connect("https://www.uberguestlist.com/");
-var guestlistMetadata = {};
 
-function guestClicked(guestId, guestName){
-	
-	/*var id = "#" + guestId;
-	$(id).hide('slow');
-	$( "#greenCheckmark" ).show( "fast", 
-		function() {setTimeout(function(){ $( "#greenCheckmark" ).hide(); }, 1000);   
-	});*/
+if(typeof io != "undefined") {
+	var socket = io.connect("https://www.uberguestlist.com/");
+	var guestlistMetadata = {};
 
-	$("#confirmDeleteGuestName").html(guestName + "?");
-	$("#confirmDeleteGuest").attr("onclick", "markGuest('" + guestId + "','"+ guestName + "')");
-	$("#popupButton").click();
-}
-
-function markGuest(guestId, guestName){
-
-		for (i = 0; i < guestlistMetadata.guests.length; i++) {
-			if(guestId == guestlistMetadata.guests[i].fakeid) {
-				// Send socket to remove
-			}
-		}
-
-		var id = "#" + guestId;
+	function guestClicked(guestId, guestName){
+		
+		/*var id = "#" + guestId;
 		$(id).hide('slow');
 		$( "#greenCheckmark" ).show( "fast", 
 			function() {setTimeout(function(){ $( "#greenCheckmark" ).hide(); }, 1000);   
-		});
-		$("#cancelDeleteGuest").click();    
-}
+		});*/
 
-// Show pass windows if server is online
-socket.on("connect", function(err) {
-	console.log("Connection is online")
-	$("#enterEventPass").show();
-	if($("#eventPassword").val() != "") {
-		$("#eventButton").click();
+		$("#confirmDeleteGuestName").html(guestName + "?");
+		$("#confirmDeleteGuest").attr("onclick", "markGuest('" + guestId + "','"+ guestName + "')");
+		$("#popupButton").click();
 	}
-});
 
-// 
-socket.on("disconnect", function(err) {
-	console.log("Lost connection")
-	$("#event").hide();
-	$("#enterEventPass").show();
-	$(".ui-controlgroup-controls").empty();
-});
+	function markGuest(guestId, guestName){
 
-$("#eventButton").click(function() {
-	socket.emit("getEvent", {eventId: $("#eventPassword").val()} , function(eventData) {
-		if(eventData.code == 1) {
-			// Event wasn't found
-			$("#enterEventError").text("Event wasn't found");
+			for (i = 0; i < guestlistMetadata.guests.length; i++) {
+				if(guestId == guestlistMetadata.guests[i].fakeid) {
+					// Send socket to remove
+				}
+			}
+
+			var id = "#" + guestId;
+			$(id).hide('slow');
+			$( "#greenCheckmark" ).show( "fast", 
+				function() {setTimeout(function(){ $( "#greenCheckmark" ).hide(); }, 1000);   
+			});
+			$("#cancelDeleteGuest").click();    
+	}
+
+	// Show pass windows if server is online
+	socket.on("connect", function(err) {
+		console.log("Connection is online");
+		$("#loadingPage").hide();
+		$("#enterEventPass").show();
+		if($("#eventPassword").val() != "") {
+			$("#eventButton").click();
 		}
-		else {
-			guestlistMetadata = {
-				eventName: eventData.name,
-				guests: eventData.guests
-			};
+	});
 
-			document.addEventListener("deviceready", onDeviceReady, false);
-			var isDeviceReady = false;
-			function onDeviceReady(){
-				if(!isDeviceReady){
-					isDeviceReady = true;
-					$("#eventName").html(guestlistMetadata.eventName);
-					if(guestlistMetadata && guestlistMetadata.guests && guestlistMetadata.guests.length>0){
-						var guests = guestlistMetadata.guests;
-						for (i = 0; i < guests.length; i++) {
-							var newId = Math.floor(Math.random() * (9999999 - 1111111) + 1111111);
-							guestlistMetadata.guests[i].fakeid = newId;
-							guests[i].fakeid = newId;
-							var guest= $('<a id="'+ guests[i].fakeid +'" href="#" class="ui-btn ui-shadow ui-corner-all" onclick="guestClicked(\'' + guests[i].fakeid + '\',\''+ guests[i].Name + '\')">' + guests[i].Name + '</a>');
-							$(".ui-controlgroup-controls ").append(guest);
+	// 
+	socket.on("disconnect", function(err) {
+		console.log("Lost connection")
+		$("#event").hide();
+		$("#enterEventPass").hide();
+		$("#loadingPage").show();
+		$(".ui-controlgroup-controls").empty();
+	});
+
+	$("#eventButton").click(function() {
+		socket.emit("getEvent", {eventId: $("#eventPassword").val()} , function(eventData) {
+			if(eventData.code == 1) {
+				// Event wasn't found
+				$("#enterEventError").text("Event wasn't found");
+			}
+			else {
+				guestlistMetadata = {
+					eventName: eventData.name,
+					guests: eventData.guests
+				};
+
+				document.addEventListener("deviceready", onDeviceReady, false);
+				var isDeviceReady = false;
+				function onDeviceReady(){
+					if(!isDeviceReady){
+						isDeviceReady = true;
+						$("#eventName").html(guestlistMetadata.eventName);
+						if(guestlistMetadata && guestlistMetadata.guests && guestlistMetadata.guests.length>0){
+							var guests = guestlistMetadata.guests;
+							for (i = 0; i < guests.length; i++) {
+								var newId = Math.floor(Math.random() * (9999999 - 1111111) + 1111111);
+								guestlistMetadata.guests[i].fakeid = newId;
+								guests[i].fakeid = newId;
+								var guest= $('<a id="'+ guests[i].fakeid +'" href="#" class="ui-btn ui-shadow ui-corner-all" onclick="guestClicked(\'' + guests[i].fakeid + '\',\''+ guests[i].Name + '\')">' + guests[i].Name + '</a>');
+								$(".ui-controlgroup-controls ").append(guest);
+							}
 						}
 					}
 				}
+				
+				$("#enterEventPass").hide();
+				$("#enterEventError").text("");
+				$("#event").show();
+
 			}
-			
-			$("#enterEventPass").hide();
-			$("#enterEventError").text("");
-			$("#event").show();
-
-		}
+		});
 	});
-});
-
+}
+else {
+	setTimeout(function(){
+		location.reload();
+		console.log("Reconnecting");
+	},5000);
+}
