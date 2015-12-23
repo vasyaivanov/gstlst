1a9793783aa64743112d3ab96115465d75a1eb57
Konstantin <kraskoshnyy@gmail.com>
2015-12-22 19:55:57 -0800

    Majos socket fixe

--- APP/gstlst/platforms/ios/www/js/index.js
+++ APP/gstlst/platforms/ios/www/js/index.js
@@ -1,10 +1,12 @@
+var appObj = new Object();
 
+$( document ).ready(function() {
 if(typeof io != "undefined") {
 	var socket = io.connect("https://www.uberguestlist.com/");
 	//var socket = io.connect("http://www.ugl.loc/");
 	var guestlistMetadata = {};
 
-	// Functionality: hiding list before an orginized enters guest's name
+	// Functionality: hiding list before the orginized enters guest's name
 	/*$("#filterControlgroup-input").on("keydown", function (e) {
 		console.log($("#filterControlgroup-input").val().length);
 		if($("#filterControlgroup-input").val().length >= 1) {
@@ -14,21 +16,21 @@ if(typeof io != "undefined") {
 			$(".ui-controlgroup-controls").hide();
 		}
 	});*/
-	
-	function guestClicked(guestId, guestName){
-		
+
+	appObj.guestClicked = function (guestId, guestName){
+
 		/*var id = "#" + guestId;
 		$(id).hide('slow');
-		$( "#greenCheckmark" ).show( "fast", 
-			function() {setTimeout(function(){ $( "#greenCheckmark" ).hide(); }, 1000);   
+		$( "#greenCheckmark" ).show( "fast",
+			function() {setTimeout(function(){ $( "#greenCheckmark" ).hide(); }, 1000);
 		});*/
 
 		$("#confirmDeleteGuestName").html(guestName + "?");
-		$("#confirmDeleteGuest").attr("onclick", "markGuest('" + guestId + "','"+ guestName + "')");
+		$("#confirmDeleteGuest").attr("onclick", "appObj.markGuest('" + guestId + "','"+ guestName + "')");
 		$("#popupButton").click();
 	}
 
-	function markGuest(guestId, guestName){
+	appObj.markGuest = function(guestId, guestName){
 			for (i = 0; i < guestlistMetadata.guests.length; i++) {
 				if(guestId == guestlistMetadata.guests[i].fakeid) {
 					// Send socket to remove
@@ -39,15 +41,15 @@ if(typeof io != "undefined") {
 						}
 					});
 				}
-			} 
+			}
 	}
-	
+
 	function removeFromList(guestId,showMark) {
 		var id = "#" + guestId;
 		$(id).hide('slow');
 		if(showMark == 1) {
-			$( "#greenCheckmark" ).show( "fast", 
-				function() {setTimeout(function(){ $( "#greenCheckmark" ).hide(); }, 1000);   
+			$( "#greenCheckmark" ).show( "fast",
+				function() {setTimeout(function(){ $( "#greenCheckmark" ).hide(); }, 1000);
 			});
 		}
 		$("#cancelDeleteGuest").click();
@@ -56,17 +58,26 @@ if(typeof io != "undefined") {
 	// Show pass windows if server is online
 	socket.on("connect", function(err) {
 		console.log("Connection is online");
-		$("#loadingPage").hide();
-		$("#enterEventPass").show();
+		if($("#eventPassword").val() == "") {
+			$("#loadingPage").hide();
+			$("#enterEventPass").show();
+		}
+		//if($(""))
 	});
 
-	// 
+	//
 	socket.on("disconnect", function(err) {
 		console.log("Lost connection")
 		$("#event").hide();
 		$("#enterEventPass").hide();
 		$("#loadingPage").show();
 		$(".ui-controlgroup-controls").empty();
+	});
+
+
+	socket.on("reconnect", function(err) {
+		console.log("Reconnected");
+		loadList();
 	});
 
 	socket.on("markedUser", function(data) {
@@ -76,8 +87,13 @@ if(typeof io != "undefined") {
 			}
 		}
 	});
-	
+
 	$("#eventButton").click(function() {
+		loadList();
+	});
+
+
+	function loadList() {
 		socket.emit("getEvent", {eventId: $("#eventPassword").val()} , function(eventData) {
 			if(eventData.code == 1) {
 				// Event wasn't found
@@ -101,13 +117,14 @@ if(typeof io != "undefined") {
 								var newId = Math.floor(Math.random() * (9999999 - 1111111) + 1111111);
 								guestlistMetadata.guests[i].fakeid = newId;
 								guests[i].fakeid = newId;
-								var guest= $('<a id="'+ guests[i].fakeid +'" href="#" class="ui-btn ui-shadow ui-corner-all" onclick="guestClicked(\'' + guests[i].fakeid + '\',\''+ guests[i].Name + '\')">' + guests[i].Name + '</a>');
+								guests[i].Name = guests[i].Name.replace("'","");
+								var guest= $('<a id="'+ guests[i].fakeid +'" href="#" class="ui-btn ui-shadow ui-corner-all" onclick="appObj.guestClicked(\'' + guests[i].fakeid + '\',\''+ guests[i].Name + '\')">' + guests[i].Name + '</a>');
 								$(".ui-controlgroup-controls ").append(guest);
 							}
 						}
 					}
 				}
-				
+
 				$("#enterEventPass").hide();
 				$("#enterEventError").text("");
 				//$(".ui-controlgroup-controls ").hide();
@@ -115,7 +132,8 @@ if(typeof io != "undefined") {
 
 			}
 		});
-	});
+	}
+
 }
 else {
 	setTimeout(function(){
@@ -123,3 +141,4 @@ else {
 		console.log("Reconnecting");
 	},5000);
 }
+});
